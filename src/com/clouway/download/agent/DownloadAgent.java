f706267131317dd99a9a9b7eee13164c7ec98be4
KrasimirRaikov <raikov.krasimir@gmail.com>
2016-01-06 15:38:46 +0200

    made the DownloadAgent download jpgs successfully using tdd aproach

---
+++ src/com/clouway/download/agent/DownloadAgent.java
@@ -0,0 +1,43 @@
+package com.clouway.download.agent;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * @author Krasimir Raikov(raikov.krasimir@gmail.com)
+ */
+public class DownloadAgent {
+
+    public void downloadFile(String url) {
+        try {
+            URL url1 = new URL(url);
+            URLConnection connection = url1.openConnection();
+
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buf = new byte[2048];
+            int n;
+            while (-1 != (n = in.read(buf))) {
+                out.write(buf, 0, n);
+            }
+            FileOutputStream fileOut = new FileOutputStream(fileName(url1.getFile()));
+            fileOut.write(out.toByteArray());
+            fileOut.close();
+            in.close();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String fileName(String file) {
+        String[] files = file.split("/");
+
+        return files[files.length - 1];
+    }
+}
---
+++ test/com/clouway/download/agent/DownloadAgentTest.java
@@ -0,0 +1,39 @@
+package com.clouway.download.agent;
+
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * @author Krasimir Raikov(raikov.krasimir@gmail.com)
+ */
+public class DownloadAgentTest {
+
+    @Test
+    public void successfulImageDownload() throws IOException {
+        DownloadAgent da = new DownloadAgent();
+        da.downloadFile("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1920px-Cat_poster_1.jpg");
+
+        BufferedImage expImage =
+                ImageIO.read(new URL("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1920px-Cat_poster_1.jpg").openStream());
+        byte[] expectedImage = ((DataBufferByte) expImage.getData().getDataBuffer()).getData();
+        BufferedImage downImage = ImageIO.read(new File("1920px-Cat_poster_1.jpg"));
+        byte[] downloadedImage = ((DataBufferByte) downImage.getData().getDataBuffer()).getData();
+
+
+        assertThat(downloadedImage, is(equalTo(expectedImage)));
+    }
+
+    
+
+
+}
