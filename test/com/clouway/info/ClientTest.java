470dee5784bc00f95cbded25f54ce8ea3c6cbefe
KrasimirRaikov <raikov.krasimir@gmail.com>
2016-02-09 09:53:08 +0200

    finally the client tests have worked.

--- test/com/clouway/info/ClientTest.java
+++ test/com/clouway/info/ClientTest.java
@@ -3,6 +3,7 @@ package com.clouway.info;
 import org.jmock.Expectations;
 import org.jmock.auto.Mock;
 import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,6 +21,8 @@ public class ClientTest {
     int port;
     Client client;
     InetAddress host;
+    FakeServer fakeServer;
+
 
 
     @Rule
@@ -33,77 +36,79 @@ public class ClientTest {
         port = 5050;
         host = InetAddress.getByName("localhost");
         client = new Client(board);
+        fakeServer= new FakeServer();
+        fakeServer.start();
     }
 
+@After
+public void tearDown(){
+    fakeServer.interrupt();
+}
 
-//    @Test
-//    public void receiveMessage() {
-//        String message = "You are number: 1";
-//
-//        Socket socket = null;
-//        sendMessageToClient(message);
-//
-//
-//
-//        context.checking(new Expectations() {{
-//            oneOf(board).printStatus(message);
-//        }});
-//        try {
-//            socket = new Socket(host, port);
-//            socket.setSoTimeout(1000);
-//
-//
-//            client.run(socket);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
+
+    @Test
+    public void receiveMessage() throws IOException {
+        String message = "You are number: 1";
+
+        Socket socket = new Socket(host, port);
+
+        fakeServer.messageClient(message);
+
+
+
+        context.checking(new Expectations() {{
+            oneOf(board).printStatus(message);
+        }});
+
+            socket.setSoTimeout(1000);
+
+
+            client.run(socket);
+
+    }
 
     @Test
     public void receiveSecondMessage() throws IOException {
-        String messageOne= "one";
-        String messageTwo= "two";
+        String messageOne = "one";
+        String messageTwo = "two";
 
-        FakeServer fakeServer= new FakeServer();
+        Socket client = new Socket(host, port);
 
-        fakeServer.run();
-        Socket client= new Socket(host, port);
+        fakeServer.messageClient(messageOne);
+        fakeServer.messageClient(messageTwo);
 
-
-        context.checking(new Expectations(){{
-        oneOf(board).printStatus(messageOne);
-        oneOf(board).printStatus(messageTwo);
+        context.checking(new Expectations() {{
+            oneOf(board).printStatus(messageOne);
+            oneOf(board).printStatus(messageTwo);
         }});
 
-            fakeServer.messageClient(messageOne);
-            fakeServer.messageClient(messageTwo);
 
-            client.setSoTimeout(1000);
+        client.setSoTimeout(1000);
 
-            this.client.run(client);
+        this.client.run(client);
+
 
     }
 
-private class FakeServer extends Thread{
+    private class FakeServer extends Thread {
 
-    private PrintWriter out;
+        private PrintWriter out;
 
-    @Override
-    public void run(){
-        try (ServerSocket serverSocket= new ServerSocket(port, 1, host)){
-            Socket clientSocket= serverSocket.accept();
-            out=new PrintWriter(clientSocket.getOutputStream(), true);
+        @Override
+        public synchronized void run() {
+            try (ServerSocket serverSocket = new ServerSocket(port, 1, host)) {
+                Socket clientSocket = serverSocket.accept();
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
 
-    public void messageClient(String message){
-        out.println(message);
-    }
-    
+        public synchronized void messageClient(String message) {
+            out.println(message);
+        }
+
     }
 
 }
